d91f164076e29db6ca120080206b1e75392d1851
Thiago Croix <tarquiano_crz@hotmail.com.br>
2018-07-19 15:15:15 -0300

    Primeiro commit com todos os arquivos

---
+++ js/imagem.js
@@ -0,0 +1,11 @@
+function colocarImagem (){
+    var j =   document.createElement("IMG");
+    j.src= "./img/titulo.png";
+    document.getElementById("titulo").appendChild(j);
+    
+    var i =   document.createElement("IMG");
+   i.src= "./img/mascara.png";
+   document.getElementById("mascara").appendChild(i);
+     
+  
+}
